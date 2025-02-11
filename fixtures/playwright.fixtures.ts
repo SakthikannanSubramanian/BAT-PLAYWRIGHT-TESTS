8148e95c81a6b38e93bebc56e292f9ea9aec9831
manoj150694 <manojethiraj25@gmail.com>
2025-02-11 21:58:50 +0000

    added search page and e2e tests

--- fixtures/playwright.fixtures.ts
+++ fixtures/playwright.fixtures.ts
@@ -1,16 +1,21 @@
 import { test as base } from '@playwright/test';
 import { LoginPage } from '../frontend/pages/loginPage'; 
+import { SearchPage } from '../frontend/pages/searchPage'; 
 import { APIHelper } from '../backend/utils/apiHelper';
 import { Helper } from '../frontend/pages/helper';
 import { GraphQLHelper } from '../graphQL/utils/graphQLHelper';
 import { allure } from 'allure-playwright';
 
-export const test = base.extend<{ loginPage: LoginPage, apiHelper: APIHelper,  
+export const test = base.extend<{ loginPage: LoginPage, searchPage: SearchPage, apiHelper: APIHelper,  
   graphqlHelper: GraphQLHelper, uiHelper: Helper, allure: typeof allure}>({
 
   loginPage: async ({ page }, use) => {
     const loginPage = new LoginPage(page);
     await use(loginPage);
+  },
+  searchPage : async ({ page }, use) => {
+    const searchPage = new SearchPage(page);
+    await use(searchPage);
   },
   uiHelper: async ({ page }, use) => {
     const uiHelper = new Helper(page);
